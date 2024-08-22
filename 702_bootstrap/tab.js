bfaf2e60f7ba284358d7780560bd4b3268090c4e
neogoolboss <krossjsnet88@gmail.com>
2024-08-22 22:15:54 +0900

    이벤트버블링, dataset

--- 702_bootstrap/tab.js
+++ 702_bootstrap/tab.js
@@ -20,12 +20,23 @@
 //     $('.tab-content').eq(2).addClass('show');
 // });
 
-for (let index = 0; index < $('.tab-button').length; index++) {
-    $('.tab-button').eq(index).on('click', function() {
-        $('.tab-button').removeClass('orange');
-        $('.tab-button').eq(index).addClass('orange');
-        $('.tab-content').removeClass('show');
-        $('.tab-content').eq(index).addClass('show');
-    });
+// for (let index = 0; index < $('.tab-button').length; index++) {
+//     $('.tab-button').eq(index).on('click', function() {
+//        탭열기(index);
+//     });
     
+// }
+
+
+
+$('.list').click(function(e) {
+    탭열기(e.target.dataset.id)
+    console.log(e.target.dataset)
+})
+
+function 탭열기(숫자) {
+    $('.tab-button').removeClass('orange');
+    $('.tab-button').eq(숫자).addClass('orange');
+    $('.tab-content').removeClass('show');
+    $('.tab-content').eq(숫자).addClass('show');
 }
